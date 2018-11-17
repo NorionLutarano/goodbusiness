db6be65ccedcd8247650d5325844a00a2521f670
andre luiz <andre.luiz.tavares@bol.com.br>
2018-11-17 13:07:44 -0200

    corrigir arquivos

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  ?>
 <html>
 <head>
-  <title>Goo d Businnes</title>
+  <title>Good Businnes</title>
  
   <link href="https://fonts.googleapis.com/css?family=Poppins|Muli|Paytone+One|Roboto" rel="stylesheet">
   <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap-reboot.css">
